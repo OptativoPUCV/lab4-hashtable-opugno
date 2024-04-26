6ef3cde8b771b6c4a095b8d6b0372ea50abc0fd0
opugno <163350572+opugno@users.noreply.github.com>
2024-04-26 16:32:22 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -109,15 +109,13 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map) 
 {
-  map -> current = 0;
-  while (map -> current < map -> capacity)
+  for (map -> current = 0; map -> current < map -> capacity; map -> current++)
   {
-    if (map -> buckets[map -> current] != NULL )
+    if (map -> buckets[map -> current] != NULL && map -> buckets[map -> current] -> key != NULL)
     {
       return map -> buckets[map -> current];
     }
-    map -> buckets[map -> current] = NULL;
-    map -> current ++;
+      
   }
   
   return NULL;
