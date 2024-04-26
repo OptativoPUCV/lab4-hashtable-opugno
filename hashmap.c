2fb2654a3c1c533108c08b0d47f0e95592df7249
opugno <163350572+opugno@users.noreply.github.com>
2024-04-26 15:31:43 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -115,7 +115,7 @@ Pair * firstMap(HashMap * map)
     return map -> buckets[map -> current];
     //map -> current ++;
   }
-  //map -> buckets[map -> current] = NULL;
+  map -> buckets[map -> current] = NULL;
   map -> current ++;
   return NULL;
 }
